94cd8cc28a51ffe07df619d88377cd48be13c6ec
soyeun <thduswkd96@gmail.com>
2025-10-26 22:56:02 +0900

    feat(main): 입력된 자동자 이름을 쉼표로 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,10 +2,19 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNames = Console.readLine();
+
+        String[] nameSplit = splitCarNamesByComma(carNames);
+        System.out.println(Arrays.toString(nameSplit));
+    }
+
+    public static String[] splitCarNamesByComma(String carNames) {
+        return carNames.split(",");
     }
 }
