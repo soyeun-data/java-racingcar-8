3e3a76645a2d85d592ab25445e11ad989f94298e
soyeun <thduswkd96@gmail.com>
2025-10-27 16:35:43 +0900

    refactor(RacingGame): MoveStrategy와 FindWinners를 묶어서 실행하는 로직

---
+++ src/main/java/racingcar/model/RacingGame.java
@@ -0,0 +1,30 @@
+package racingcar.model;
+
+import racingcar.view.Output;
+
+import java.util.List;
+
+public class RacingGame {
+    private final Cars cars;
+    private final int attemptCnt;
+    private final MoveStrategy moveStrategy;
+    private final Output output;
+
+    public RacingGame(Cars cars, int attemptCnt, MoveStrategy moveStrategy, Output output) {
+        this.cars = cars;
+        this.attemptCnt = attemptCnt;
+        this.moveStrategy = moveStrategy;
+        this.output = output;
+    }
+
+    public void play() {
+        for (int i = 0; i < attemptCnt; i++) {
+            cars.race(moveStrategy);
+            output.printMoveResult(cars.getCars());
+        }
+
+        List<String> winners = FindWinners.findWinners(cars);
+        output.printWinner(winners);
+    }
+
+}
